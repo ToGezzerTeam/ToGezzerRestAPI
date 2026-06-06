e0b0ab542a2973400abdd252b090080ea72f41f9
Arthur FRIN <frin.arthur@gmail.com>
2026-06-06 23:15:36 +0200

    fix:cors

--- src/main/java/com/togezzer/restapi/config/SecurityConfig.java
+++ src/main/java/com/togezzer/restapi/config/SecurityConfig.java
@@ -52,7 +52,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of(allowedOrigins));
+        // config.setAllowedOrigins(List.of(allowedOrigins)); // prod
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST","PATCH", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
